9ca6631a815473d86b51abab661860eb42e3af1d
vyshnavirakvih-cyber <vyshnavi.rakvih@gmail.com>
2026-01-24 16:38:48 +0530

    Finally Code

--- android/app/src/main/java/com/instafitcore/app/MainActivity.java
+++ android/app/src/main/java/com/instafitcore/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.instafitcore.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Capacitor handles the loading of the URL set in capacitor.config.ts automatically
+    }
+}
