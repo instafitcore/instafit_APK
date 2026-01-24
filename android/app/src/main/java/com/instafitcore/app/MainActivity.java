33d86b7682cb0ea7789071dc8d39f5dd938fca06
vyshnavirakvih-cyber <vyshnavi.rakvih@gmail.com>
2026-01-24 12:23:19 +0530

    APk file

--- android/app/src/main/java/com/instafitcore/app/MainActivity.java
+++ android/app/src/main/java/com/instafitcore/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.instafitcore.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',      // <--- THIS IS THE MISSING KEY
   trailingSlash: true,
   images: {
     unoptimized: true,
